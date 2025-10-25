60460dc4f4e252e4aec82c93c234c24bae97c7d5
Marissa O'Meally <marissaomeally21@gmail.com>
2025-10-25 18:32:43 -0500

    Avengers Character Search Completed

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 // Wait for the page to finish load
 document.addEventListener("DOMContentLoaded", function(){
-    const Searchbtn = document.getElementById("btn");
+    const form = document.getElementById("SearchForm");
+    const results = document.getElementById("result");
+
+    function fetch_Superheroes(seacrhData=""){
+
+        // Sanitize user input
+        const userInput = seacrhData.replace(/</g, "&lt;").replace(/>/g, "&gt;").trim();
+
+        fetch("superheroes.php?query=" + encodeURIComponent(userInput))   // AJAX request
+            .then(response => response.json())
+            .then(data => {
+
+                if(Array.isArray(data)){
+                    let htmlContent = "<ul>";
+                    data.forEach(superhero =>{
+                        htmlContent += `<li>${superhero.alias}</li>`;
+                    });
+                    htmlContent +="</ul>";  
+                    results.innerHTML= htmlContent;
+                }
+
+                // If a single superhero is searched
+                else if(data && data.alias){
+                    results.innerHTML = `
+                        <h3>${data.alias}</h3>
+                        <h4>A.K.A ${data.name}</h4>
+                        <p>${data.biography}</p>
+                    `;
+                }     
+                else{
+                    results.innerHTML = `<p class="not-found">Superhero not found</p>`;  // If no superhero is found
+                }
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert("Error in fetching Superheroes.");   // Error alert message
+            });
+    }    
+
+    // Fetch Superheroes
+    fetch_Superheroes();
 
     // When the Search Button is clicked
-    Searchbtn.addEventListener("click", function(){
-        fetch("superheroes.php")   // AJAX request
-        .then(response => response.text())
-        .then(data => {
-            alert(data);   // Show result as an alert
-        })
-        .catch(error => {
-           console.error("Error:", error);
-            alert("Error in fetching Superheroes.");   // Error alert message
-        });
-    });
+    form.addEventListener("submit", function(event){
+        event.preventDefault();  // prevent page from reloading
+        const seacrhData = document.getElementById('search').value;
+        fetch_Superheroes(seacrhData);
+    });      
+
 });
-        
-
-
